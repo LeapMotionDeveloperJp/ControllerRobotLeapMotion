304c40a9045754d3fafb7a1065ac739ce06e609c
bookair17 <kyoji-exterior@goo.jp>
2014-11-22 20:43:43 +0900

    Update : http request

--- forRaspberryPi/server.js
+++ forRaspberryPi/server.js
@@ -1,34 +1,74 @@
 require('leapjs/template/entry');
+require('leapjs-plugins/main/hand-entry/leap.hand-entry');
 
+var request = require('request');
 var frameCount = 0;
-var controller = new Leap.Controller();
+var position;
+var controller = new Leap.Controller()
+//    .use('handEntry')
+    ;
+var rightFlag = false;
+var leftFlag = false;
+var rightGpio = "http://192.168.0.20:8000/GPIO/18/";
+var leftGpio = "http://192.168.0.20:8000/GPIO/24/";
+
 controller.on("frame", function (frame) {
 //    console.log("Frame: " + frame.id + " @ " + frame.timestamp);
+    console.log("frame.hands: " + frame.hands);
+
     if(frame.hands.length > 0)
     {
-        var hand = frame.hands[0];
-        var position = hand.palmPosition;
+        var hand = frame.hands;
+        position = hand.palmPosition;
         var velocity = hand.palmVelocity;
         var direction = hand.direction;
         var type = hand.type;
+        var depth = 120;
+
         if(type == "left"){
-            console.log("Frame: " + frame.id + " @ " + frame.timestamp);
-            console.log("Left hand.");
+            if(position[1] > depth){
+                leftFlag = true;
+                console.log("position[1]: " + position[1]);
+                console.log("Frame: " + frame.id + " @ " + frame.timestamp);
+                console.log("Left hand.")
+            }
         } else {
-            if(position[2] < -120){
+            if(position[1] > depth){
+                rightFlag = true;
+                console.log("position[1]: " + position[1]);
                 console.log("Frame: " + frame.id + " @ " + frame.timestamp);
                 console.log("Right hand.")
             }
         }
+    }else{
+        if(rightFlag){
+            // 右パンチイベント発火
+            request.post(rightGpio + "function/out");
+            request.post(rightGpio + "value/1");
+            rightFlag = false;
+            setTimeout(function(){
+                request.post(rightGpio + "value/0");
+            },1000);
+        }else if(leftFlag){
+            // 左パンチイベント発火
+            request.post(leftGpio + "function/out");
+            request.post(leftGpio + "value/1");
+            leftFlag = false;
+            setTimeout(function(){
+                request.post(leftGpio + "value/0");
+            },1000);
+        }
     }
+//    position = null;
 });
 
 //
-//setInterval(function () {
+setInterval(function () {
+    console.log("position: " + position);
 //    var time = frameCount / 2;
 //    console.log("received " + frameCount + " frames @ " + time + "fps");
 //    frameCount = 0;
-//}, 2000);
+}, 2000);
 
 controller.on('ready', function () {
     console.log("ready");
