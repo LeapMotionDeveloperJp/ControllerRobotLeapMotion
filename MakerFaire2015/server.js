6c0d84207e2c05100647e004d89e8798a8794790
K90j1 <kyoji-exterior@goo.jp>
2015-07-11 05:04:28 +0900

    rename

--- MakerFaire2015/server.js
+++ MakerFaire2015/server.js
@@ -1,68 +1,87 @@
+/**
+ * LeapMotion Server
+ * @module ws
+ * @module request
+ * @module leapjs
+ */
 require('leapjs/template/entry');
-require('leapjs-plugins/main/transform/leap.transform');
+var controller = new Leap.Controller();
 var request = require('request');
-var controller = new Leap.Controller()
-    .use('transform', {
-        vr: true
-        })
-    ;
-var position = [];
-var rightGpioUrl = "http://192.168.0.20:8000/GPIO/24/";
-var distance = -120;
-var scale = 500;
-var upEvent = true;
+const GPIO_URL = "http://192.168.0.20:8000/GPIO/24/";
+const DISTANCE = -120;
+const SCALE = 500;
+var isUpEvent = true;
+var isGaming = true;
 
-controller.on("frame", function (frame) {
-    var handsLength = frame.hands.length;
-    console.log(handsLength);
-    if (handsLength == 2) {
-        upEvent = true;
-        console.log("UP");
-    }
-    if ((upEvent) && handsLength == 0){
-        console.log("Down");
-        upEvent = false;
-        request
-            .post(rightGpioUrl + "function/out")
-            .on('error', function(err) {
-                console.log(err)
-            });
-        request
-            .post(rightGpioUrl + "value/1")
-            .on('error', function(err) {
-                console.log(err)
-            });
-        setTimeout(function () {
+var actionLeapMotion = function () {
+    controller.on("frame", function (frame) {
+        if(!isGaming)
+            return;
+        var handsLength = frame.hands.length;
+        if (handsLength == 2) {
+            isUpEvent = true;
+            console.log("UP");
+        }
+        if ((isUpEvent) && handsLength == 0) {
+            console.log("Down");
+            isUpEvent = false;
             request
-                .post(rightGpioUrl + "value/0")
-                .on('error', function(err) {
+                .post(GPIO_URL + "function/out")
+                .on('error', function (err) {
                     console.log(err)
                 });
-        }, scale);
-    }
-});
+            request
+                .post(GPIO_URL + "value/1")
+                .on('error', function (err) {
+                    console.log(err)
+                });
+            setTimeout(function () {
+                request
+                    .post(GPIO_URL + "value/0")
+                    .on('error', function (err) {
+                        console.log(err)
+                    });
+            }, SCALE);
+        }
+    });
 
-controller.on('ready', function () {
-    console.log("ready");
-});
-controller.on('connect', function () {
-    console.log("connect");
-});
-controller.on('disconnect', function () {
-    console.log("disconnect");
-});
-controller.on('focus', function () {
-    console.log("focus");
-});
-controller.on('blur', function () {
-    console.log("blur");
-});
-controller.on('deviceConnected', function () {
-    console.log("deviceConnected");
-});
-controller.on('deviceDisconnected', function () {
-    console.log("deviceDisconnected");
-});
+    controller.on('ready', function () {
+        console.log("ready");
+    });
+    controller.on('connect', function () {
+        console.log("connect");
+    });
+    controller.on('disconnect', function () {
+        console.log("disconnect");
+    });
+    controller.on('focus', function () {
+        console.log("focus");
+    });
+    controller.on('blur', function () {
+        console.log("blur");
+    });
+    controller.connect();
+    console.log("\nWaiting for device to connect...");
+}
 
-controller.connect();
-console.log("\nWaiting for device to connect...");
+var WebSocketServer = require('ws').Server
+    , wss = new WebSocketServer({port: 8080});
+wss.on('connection', function (ws) {
+    ws.on('message', function (message) {
+        if (message == "start") {
+            if(isGaming){
+                actionLeapMotion();
+            }else{
+                isGaming = true;
+            }
+            console.log('received: game %s', message);
+            console.log("\nWaiting for device to connect...");
+        } else if(message == "stop"){
+            isGaming = false;
+            console.log("received: game stop");
+            ws.send('server_stop');
+        } else {
+            console.log("Type to run \"node Controller.js\"")
+        }
+    });
+});
